88b43a87c9ae3dc7209b006aaf5367e53b2da9db
Lutz Reinhardt <y0032316@izcip10.(none)>
2012-02-17 00:17:01 +0100

    add comments to functions

--- src/gpu-mc.h
+++ src/gpu-mc.h
@@ -1,4 +1,10 @@
-
+// loads voxeldata onto the gpu and frees the voxels pointer
+// voxels needs to be cubic with sidelength of a power of 2
 void setupCuda(unsigned char * voxels, unsigned int size);
+
+// classifies each voxel and calculates the number of triangles needed for this
+// voxel
 void updateScalarField();
+
+// calculates the total number of triangles needed
 void histoPyramidConstruction();
