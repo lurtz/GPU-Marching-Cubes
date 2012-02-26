1e9d9aaaa8341c6089f5b0afe5bda77b76874d13
Lutz Reinhardt <y0032316@izcip10.(none)>
2012-02-26 19:07:53 +0100

    use histopyramid tests

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,7 @@ int main(int argc, char * argv[]) {
   success &= testHistoPyramidConstruction();
 
   histoPyramidTraversal();
+  success &= testHistoPyramidTraversal();
 //  run();
 
   std::cout << "no segfault here" << std::endl;
