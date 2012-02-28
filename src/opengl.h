1f5ed5f2add879c18a1108a2c25f590eec6737df
Lutz Reinhardt <y0032316@izcip07.(none)>
2012-02-28 14:28:50 +0100

    add inclde guard

--- src/opengl.h
+++ src/opengl.h
@@ -1,4 +1,7 @@
-
+#ifndef __OPENGL_H__
+#define __OPENGL_H__
 
 void setupOpenGL(int * argc, char ** argv, int size, int sizeX, int sizeY, int sizeZ, float spacingX, float spacingY, float spacingZ);
 void run();
+
+#endif // __OPENGL_H__
